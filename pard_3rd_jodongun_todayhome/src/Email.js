5853d451065d99d86e7f801128ba02e9d8932073
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-03-28 18:03:28 +0900

    add: Email.js

--- pard_3rd_jodongun_todayhome/src/Email.js
+++ pard_3rd_jodongun_todayhome/src/Email.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function Email(){
     return(
-        <div className="email-frame">
+        <div className="form-email">
             이메일
             <input className="email-input" type="text" placeholder="이메일"/>
         </div>
