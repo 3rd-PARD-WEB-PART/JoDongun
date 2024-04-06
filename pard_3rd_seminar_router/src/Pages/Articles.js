e18d72ea3427b3ef4dfdd3e384c02d54f938b29d
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-04-06 16:26:56 +0900

    recommit

--- pard_3rd_seminar_router/src/Pages/Articles.js
+++ pard_3rd_seminar_router/src/Pages/Articles.js
@@ -8,8 +8,13 @@ function Articles() {
     return (
         <div>
             <Outlet/> 
-            {/* 자식 컴포넌트를 보여주는 얘 */}
             <ul>
+                <ArticleItem id={1}/>
+                <ArticleItem id={2}/>
+                <ArticleItem id={3}/>
+            </ul>
+            {/* 자식 컴포넌트를 보여주는 얘 */}
+            {/* <ul>
                 <li>
                     <NavLink
                         to="/articles/1"
@@ -19,18 +24,42 @@ function Articles() {
                                 : undefined
                         )}>게시글 1</NavLink>
                 </li>
-                {/* <li>
+                <li>
                     <Link to="/articles/1">게시글 1</Link>
-                </li> */}
+                </li>
                 <li>
                     <Link to="/articles/2">게시글 2</Link>
                 </li>
                 <li>
                     <Link to="/articles/3">게시글 3</Link>
                 </li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
+
+const ArticleItem = ({id}) => {
+    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+    };
+    const defaultStyle = {
+        color: 'black',
+        fontSize: 15,
+    }
+    return(
+        <li>
+            <NavLink
+                to={`/articles/${id}`}
+                style={({isActive}) => (isActive ? activeStyle : defaultStyle)}>
+                    게시글{id}
+                </NavLink>
+        </li>
+    )
+}
+
+// const MyNavLink = styled(NavLink)`
+// 스타일 작성 -> 나중에 이게 undefined로 된다.3
+// `
 
 export default Articles;
