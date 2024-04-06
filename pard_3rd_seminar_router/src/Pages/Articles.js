37d05d5138c6330999a03455c66b71f23c1b22d5
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-04-06 16:05:39 +0900

    recommit

--- pard_3rd_seminar_router/src/Pages/Articles.js
+++ pard_3rd_seminar_router/src/Pages/Articles.js
@@ -1,14 +1,27 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 function Articles() {
+    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+    }
     return (
         <div>
             <Outlet/> 
             {/* 자식 컴포넌트를 보여주는 얘 */}
             <ul>
                 <li>
-                    <Link to="/articles/1">게시글 1</Link>
+                    <NavLink
+                        to="/articles/1"
+                        style={({isActive}) => (
+                            isActive ?
+                                activeStyle
+                                : undefined
+                        )}>게시글 1</NavLink>
                 </li>
+                {/* <li>
+                    <Link to="/articles/1">게시글 1</Link>
+                </li> */}
                 <li>
                     <Link to="/articles/2">게시글 2</Link>
                 </li>
